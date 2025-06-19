b0206b50c06c7c4265db0b529d715b18989ff476
DDstar1 <abhuluimendesiny@gmail.com>
2025-06-19 12:10:40 +0100

    k

--- components/CelebCard.jsx
+++ components/CelebCard.jsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 
 const CelebCard = ({ celeb }) => (
-  <div className=" bg-white relative rounded-2xl shadow-lg overflow-hidden w-full aspect-square">
-    <Image src={celeb.image} alt={celeb.name} layout="fill" objectFit="cover" />
-    <div className="absolute bottom-0 left-0 w-full h-fit p-2 bg-gradient-to-t from-black/60 to-transparent">
+  <div className="bg-white relative rounded-2xl shadow-lg overflow-hidden w-full aspect-square">
+    <img
+      src={celeb.image}
+      alt={celeb.name}
+      className="absolute inset-0 w-full h-full object-cover"
+    />
+    <div className="absolute bottom-0 left-0 w-full h-fit pt-6 p-2 bg-gradient-to-t from-black/80 to-transparent">
       <h2 className="text-lg text-white font-semibold">{celeb.name}</h2>
     </div>
   </div>
