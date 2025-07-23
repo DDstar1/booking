6fe20e754268efe8a1a065c6341f3f831f737422
DDstar1 <abhuluimendesiny@gmail.com>
2025-07-23 17:10:17 +0100

    scc

--- app/ctrl_access_874/page.js
+++ app/ctrl_access_874/page.js
@@ -87,7 +87,7 @@ export default function AdminPanel() {
   return (
     <div className="bg-gradient-to-br from-gray-50 to-gray-100 min-h-screen px-4 py-10">
       <div className="max-w-5xl mx-auto bg-white rounded-3xl shadow-xl p-8 space-y-8 border border-gray-200">
-        <h1 className="text-3xl font-bold text-center text-gray-800 mb-8 bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+        <h1 className="text-3xl font-bold text-center text-gray-800 mb-8 bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text ">
           Admin Panel
         </h1>
 
